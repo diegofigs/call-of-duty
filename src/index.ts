f48773d5ea6e32c2b16fbab1df194f71e836f936
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-07-30 23:28:40 -0400

    fix: fix user module export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * as Warzone from "./wz";
 export * as ModernWarfare from "./mw";
 export * as ColdWar from "./cw";
 export * as Vanguard from "./vg";
+export * as Me from "./user";
 export * from "./types";
 export { login } from "./api";
