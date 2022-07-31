0cf9154394783d22ecdc5c912d87791032e4cff4
diegofigs <12281609+diegofigs@users.noreply.github.com>
2022-07-30 23:42:28 -0400

    feat: add store module

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * as ModernWarfare from "./mw";
 export * as ColdWar from "./cw";
 export * as Vanguard from "./vg";
 export * as Me from "./user";
+export * as Store from "./store";
 export * from "./types";
 export { login } from "./api";
